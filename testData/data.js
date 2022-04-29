da1886719db39d6a0b239ea0132147e1a329324d
TrackerPro3 <96574665+TrackerPro3@users.noreply.github.com>
2022-04-29 06:14:14 +0530

    Update data.js

--- testData/data.js
+++ testData/data.js
@@ -3,6 +3,6 @@ module.exports = {
     login : {
       //  buildURL: "https://azrtracker01.trackerpro.cc/Oscar/Authentication/Login.aspx",
         userID: "srikanthyadav.kompally@ryan.com",
-        Password: "Password"
+        Password: "Passworrd"
     },
 }
